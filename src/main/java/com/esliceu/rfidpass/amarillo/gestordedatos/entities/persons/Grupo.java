2ff2ac0000c68ae1ae1a5546d14ca1c751ff6e3a
julink <julinksnow@gmail.com>
2019-02-20 21:26:10 +0100

    Añadida dependencia de Hibernate. Cambiando usuario y contraseña de la base de datos.

--- src/main/java/com/esliceu/rfidpass/amarillo/gestordedatos/entities/persons/Grupo.java
+++ src/main/java/com/esliceu/rfidpass/amarillo/gestordedatos/entities/persons/Grupo.java
@@ -1,12 +1,15 @@
 package com.esliceu.rfidpass.amarillo.gestordedatos.entities.persons;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Grupo {
 
     @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
     private Integer id;
 
     private Profesor profesor;
