f16f4fc74a874063362981a682281b52ff70b89e
julink <julinksnow@gmail.com>
2019-02-20 18:11:05 +0100

    First commit

---
+++ src/main/java/com/esliceu/rfidpass/amarillo/gestordedatos/GestorDeDatosApplication.java
@@ -0,0 +1,13 @@
+package com.esliceu.rfidpass.amarillo.gestordedatos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorDeDatosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestorDeDatosApplication.class, args);
+    }
+
+}
