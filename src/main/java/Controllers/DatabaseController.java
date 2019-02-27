2b0cc7a0d44a292295a770957d947c12d7126a9d
tomassebastia <sebastomassantacruz@gmail.com>
2019-02-27 17:39:48 +0100

    new database controller

---
+++ src/main/java/Controllers/DatabaseController.java
@@ -0,0 +1,14 @@
+package Controllers;
+
+import com.esliceu.rfidpass.amarillo.gestordedatos.mocks.FixatgeMock;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by tanin on 27/02/2019.
+ */
+@RestController
+public class DatabaseController {
+    FixatgeMock fixatge = new FixatgeMock();
+
+
+}
---
+++ src/main/java/com/esliceu/rfidpass/amarillo/gestordedatos/mocks/FixatgeMock.java
@@ -0,0 +1,15 @@
+package com.esliceu.rfidpass.amarillo.gestordedatos.mocks;
+
+import com.esliceu.rfidpass.amarillo.gestordedatos.entities.persons.Usuario;
+import com.esliceu.rfidpass.amarillo.gestordedatos.entities.tools.Lector;
+
+/**
+ * Created by tanin on 27/02/2019.
+ */
+public class FixatgeMock {
+    int id = 20;
+    String tipo = "entrada";
+    String data = "2019/02/27";
+    Lector lector;
+    Usuario usuario;
+}
