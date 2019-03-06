50267189e0178577efa88121216c999d17c16fab
rochismo <alexis.platino@gmail.com>
2019-03-06 18:05:05 +0100

    Fixed something related to schedules

--- src/main/java/com/esliceu/rfidpass/amarillo/gestordedatos/repositories/ProfessorSessionRepository.java
+++ src/main/java/com/esliceu/rfidpass/amarillo/gestordedatos/repositories/ProfessorSessionRepository.java
@@ -1,7 +1,13 @@
 package com.esliceu.rfidpass.amarillo.gestordedatos.repositories;
 
 import com.esliceu.rfidpass.amarillo.gestordedatos.entities.sessions.ProfessorSession;
+import com.esliceu.rfidpass.amarillo.gestordedatos.entities.sessions.Session;
+import com.esliceu.rfidpass.amarillo.gestordedatos.entities.users.Professor;
+import com.esliceu.rfidpass.amarillo.gestordedatos.entities.users.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface ProfessorSessionRepository extends CrudRepository<ProfessorSession, Integer> {
+    List<Session> findByProfessor(Professor professor);
 }
--- src/main/java/com/esliceu/rfidpass/amarillo/gestordedatos/repositories/StudentSessionRepository.java
+++ src/main/java/com/esliceu/rfidpass/amarillo/gestordedatos/repositories/StudentSessionRepository.java
@@ -1,7 +1,13 @@
 package com.esliceu.rfidpass.amarillo.gestordedatos.repositories;
 
+import com.esliceu.rfidpass.amarillo.gestordedatos.entities.sessions.Session;
 import com.esliceu.rfidpass.amarillo.gestordedatos.entities.sessions.StudentSession;
+import com.esliceu.rfidpass.amarillo.gestordedatos.entities.users.Student;
+import com.esliceu.rfidpass.amarillo.gestordedatos.entities.users.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface StudentSessionRepository extends CrudRepository<StudentSession, Integer> {
+    List<Session> findByStudent(Student student);
 }
